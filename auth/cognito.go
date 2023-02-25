713d5eb76ef5bf7f7b916d573b338f4b6a984f28
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-02-25 11:52:18 -0500

    implement golang.org/x/oauth2

--- auth/cognito.go
+++ auth/cognito.go
@@ -1,21 +1,16 @@
 package auth
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
+	"context"
 	"errors"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"golang.org/x/oauth2"
 )
 
 var poolId string
@@ -69,60 +64,34 @@ func New() (*Cognito, error) {
 }
 
 func (c *Cognito) GetCognitoTokenEndpointPayload(authorizationCode string) (CognitoPayload, error) {
-
 	poolDesc, err := c.client.DescribeUserPool(&cognito.DescribeUserPoolInput{UserPoolId: aws.String(poolId)})
 	if err != nil {
 		log.Printf("Error describing user pool: %v\n", err)
 		return CognitoPayload{}, err
 	}
-	var tokenEndpoint string
-	if poolDesc.UserPool.CustomDomain != nil {
-		fmt.Println("Found custom domain to build endpoint")
-		tokenEndpoint = "https://" + *poolDesc.UserPool.CustomDomain + "/oauth2/token"
-	} else if poolDesc.UserPool.Domain != nil {
-		fmt.Println("Found cognito domain to build endpoint")
-		tokenEndpoint = "https://" + *poolDesc.UserPool.Domain + ".auth." + region + ".amazoncognito.com/oauth2/token"
-	} else {
-		log.Printf("No domain present for user pool %v, unable to build token endpoint.\n", poolId)
-		return CognitoPayload{}, err
+	config := &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectUri,
+		Scopes:       []string{"openid", "email", "profile"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://" + *poolDesc.UserPool.Domain + ".auth." + region + ".amazoncognito.com/oauth2/authorize",
+			TokenURL: "https://" + *poolDesc.UserPool.Domain + ".auth." + region + ".amazoncognito.com/oauth2/token",
+		},
 	}
-	log.Printf("Token endpoint: %v\n", tokenEndpoint)
-
-	var authHeader []byte = []byte("Basic " + base64.StdEncoding.EncodeToString([]byte(clientId+":"+clientSecret)))
-	headerMap := map[string]string{
-		"Content-Type":  "application/x-www-form-urlencoded",
-		"Authorization": string(authHeader),
-	}
-	body := []byte(`grant_type=authorization_code&client_id=` + clientId + `&code=` + authorizationCode + `&redirect_uri=` + redirectUri)
-	statusCode, responseBody, err := postRequest(tokenEndpoint, headerMap, body)
+	token, err := config.Exchange(context.Background(), authorizationCode)
 	if err != nil {
-		log.Printf("Error calling token endpoint: %v\n", err)
+		log.Printf("Error exchanging code for token: %v\n", err)
 		return CognitoPayload{}, err
 	}
-	if statusCode != 200 {
-		return CognitoPayload{}, errors.New("error calling token endpoint: " + string(responseBody) + ": status code: " + strconv.Itoa(statusCode))
+	cognitoPayload := CognitoPayload{
+		AccessToken:  token.AccessToken,
+		RefreshToken: token.RefreshToken,
+		TokenType:    token.TokenType,
+		ExpiresIn:    int(token.Expiry.Unix()),
+		IdToken:      token.Extra("id_token").(string),
 	}
-	cognitoPayload := CognitoPayload{}
-	json.Unmarshal(responseBody, &cognitoPayload)
 	return cognitoPayload, nil
-}
-
-func postRequest(url string, headersMap map[string]string, body []byte) (statusCode int, responseBody []byte, err error) {
-	request, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		return 0, nil, err
-	}
-	for key, value := range headersMap {
-		request.Header.Set(key, value)
-	}
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		return 0, nil, err
-	}
-	defer response.Body.Close()
-	body, _ = io.ReadAll(response.Body)
-	return response.StatusCode, body, err
 }
 
 func (c *Cognito) GetUserInfo(accessToken string) (username, email string, err error) {
