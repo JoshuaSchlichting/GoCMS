844885c14009c3c687d91c3f98a8781b85e880c8
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-05 12:05:59 -0500

    rm unused PostURL

--- templates/components/clickable_table.go
+++ templates/components/clickable_table.go
@@ -5,7 +5,6 @@ import "html/template"
 type ClickableTable struct {
 	TableID      template.JS
 	Table        []map[string]interface{}
-	PostURL      string
 	CallbackFunc template.JS
 	JavaScript   template.JS
 }
