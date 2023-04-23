060e40d54febd8c7c9adc375ae92a7aa67a3fffd
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-04-23 18:03:30 -0400

    working image area with test data

--- presentation/blog.go
+++ presentation/blog.go
@@ -23,5 +23,17 @@ func (p *Presentor) CreateBlogHTML() error {
 			getSubmitResetButtonDiv(),
 		),
 		"FormID": "create-blog-form",
+		"ImageURLs": []string{
+			"https://picsum.photos/200/300?random=1",
+			"https://picsum.photos/200/300?random=2",
+			"https://picsum.photos/200/300?random=3",
+			"https://picsum.photos/200/300?random=4",
+			"https://picsum.photos/200/300?random=5",
+			"https://picsum.photos/200/300?random=6",
+			"https://picsum.photos/200/300?random=7",
+			"https://picsum.photos/200/300?random=8",
+			"https://picsum.photos/200/300?random=9",
+			"https://picsum.photos/200/300?random=10",
+		},
 	})
 }
