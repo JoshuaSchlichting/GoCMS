63e97487f2528ac6eaf19b12d2d97d7410b123e0
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-08-19 17:00:06 -0400

    get cookie exchange with kratos working

--- auth/kratos/session.go
+++ auth/kratos/session.go
@@ -7,12 +7,9 @@ import (
 	"log"
 	"net/http"
 	"net/http/cookiejar"
-	"time"
 
 	"log/slog"
 
-	"github.com/go-chi/jwtauth"
-	"github.com/google/uuid"
 	"github.com/joshuaschlichting/gocms/config"
 	"golang.org/x/net/publicsuffix"
 )
@@ -30,14 +27,30 @@ func SetLogger(l *slog.Logger) {
 	logger = l
 }
 
-// GetJWT performs the whoami request to kratos and then constructs a JWT from the response
-func GetJWT(cookie string) (string, error) {
+func GetUserInfo(orySessionCookie string) (string, string, error) {
+	logger.Debug("kratos.GetUserInfo ->")
+
+	data, err := getOryPayload(orySessionCookie)
+	if err != nil {
+		logger.Error("error getting ory payload", "error", err)
+		return "", "", err
+	}
+	_ = data
+
+	username := data["identity"].(map[string]interface{})["traits"].(map[string]interface{})["username"].(string)
+	email := data["identity"].(map[string]interface{})["traits"].(map[string]interface{})["email"].(string)
+
+	logger.Debug("kratos.GetUserInfo <-", "username", username, "email", email)
+	return username, email, nil
+}
+
+func getOryPayload(cookie string) (map[string]interface{}, error) {
 	// Create an HTTP client with a cookie jar
 	// The cookie jar is used to handle cookies automatically
 	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
 		log.Println("Error creating cookie jar:", err)
-		return "", err
+		return map[string]interface{}{}, err
 	}
 	client := &http.Client{
 		Jar: jar,
@@ -52,20 +65,21 @@ func GetJWT(cookie string) (string, error) {
 
 	if err != nil {
 		fmt.Println("Error creating request:", err)
-		return "", err
+		return map[string]interface{}{}, err
 	}
 	// log the entirety of the GET request
-	logger.Debug("kratos request", "request", req)
+	logger.Debug("GET to kratos", "request", req)
 
 	// // Set the necessary headers
 	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Origin", "http://web:8000")
+	// req.Header.Set("Origin", "http://web:8000")
 
 	// // Make the request
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error fetching %s: %v ", KRATOS_WHOAMI, err)
-		return "", err
+		logger.Error(fmt.Sprint("error fetching ", KRATOS_WHOAMI), "error", err)
+		return map[string]interface{}{}, err
 	}
 	defer resp.Body.Close()
 
@@ -73,44 +87,15 @@ func GetJWT(cookie string) (string, error) {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response:", err)
-		return "", err
+		return map[string]interface{}{}, err
 	}
 	logger.Debug("kratos response", "body", string(body))
 	// Unmarshal the response JSON (if you have a struct to unmarshal into)
 	var data map[string]interface{}
 	if err := json.Unmarshal(body, &data); err != nil {
 		fmt.Println("Error unmarshalling response:", err)
-		return "", err
+		return map[string]interface{}{}, err
 	}
-	if data["error"] != nil {
-		logger.Error("kratos response error", "error", data["error"])
-		return "", err
-	}
-
-	data["authSource"] = "kratos"
-	var tokenString string
-	//	build a JWT string from DATA
-	tokenAuth := jwtauth.New("HS256", []byte(conf.Auth.JWT.SecretKey), nil)
-	// conf.Auth.JWT.ExpirationTime add to now
-	expirationTime := time.Now().Add(time.Second * time.Duration(conf.Auth.JWT.ExpirationTime))
-
-	// "username": data["identity"].(map[string]interface{})["traits"].(map[string]interface{})["email"],
-	log.Println("data:", data)
-	// set token expiration
-	_, tokenString, _ = tokenAuth.Encode(map[string]interface{}{
-		"exp": expirationTime.Unix(),
-		"iat": time.Now().Unix(),
-		"iss": conf.Auth.JWT.Issuer + "-kratos",
-		"aud": conf.Auth.JWT.Audience,
-		"sub": conf.Auth.JWT.Subject,
-		// guid for jti
-		"jti": uuid.New().String(),
-	})
-
-	return tokenString, nil
-}
-
-func GetUserInfo(jwt string) (string, string, error) {
-
-	return "", "", nil
+	logger.Debug("kratos response", "data", data)
+	return data, nil
 }
